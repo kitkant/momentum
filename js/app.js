26e490b132cac167224eac4dd1b735643cd5aa56
Nikita Dyshkant <58398247+kitkant@users.noreply.github.com>
2022-08-19 10:03:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const time = document.querySelector('.time')
 const mainDate = document.querySelector('.date')
 const greeting = document.querySelector('.greeting')
 const textLS = document.querySelector('.name')
+const textCity = document.querySelector('.city')
+const weatherIcon = document.querySelector('.weather-icon')
 const lang = navigator.language.split('-')[0]
 const timeOfDay = {
     'en': ['Good morning', 'Good afternoon', 'Good evening', 'Good night'],
@@ -16,15 +18,12 @@ const options = {
 	}
 };
 
-fetch('https://open-weather13.p.rapidapi.com/kyiv/landon', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-const APIKEY = '434f66b8e0981877fdd0ff7e225ac11d'
+
+const APIKEY = 'bcd4c9e2298e5fe6d8112c0dd5a95dc3'
 const getTime = () =>{
     setInterval(()=>{
         const now = new Date();
-        const text = document.querySelector('.name').value
+      
       
         
         const hours = now.getHours()
@@ -50,16 +49,22 @@ const getTime = () =>{
             getTimeOfDay(3)
         if(hours === 0)
             getDate()
-        if(localStorage.getItem("text") !== text)
-            setLocalStorage(text)
+        if(localStorage.getItem("text") !== document.querySelector('.name').value)
+            setTextLocalStorage(document.querySelector('.name').value)
+        if(localStorage.getItem("city") !== document.querySelector('.city').value)
+            setCityLocalStorage(document.querySelector('.city').value)
     }, 1000)
 }
 
 const getTimeOfDay = (e) => greeting.innerHTML = timeOfDay[lang][e] 
 
-const setLocalStorage = event =>{
+const setTextLocalStorage = event =>{
     localStorage.setItem('text', event)
-    console.log('complete', localStorage.getItem("text"))
+    console.log('complete text', localStorage.getItem("text"))
+}
+const setCityLocalStorage = event =>{
+    localStorage.setItem('city', event)
+    console.log('complete city', localStorage.getItem("city"))
 }
 
 const getDate = () =>{
@@ -69,9 +74,22 @@ const getDate = () =>{
 }
 
 function app(){
+    fetch('http://api.weatherstack.com/current?access_key=bcd4c9e2298e5fe6d8112c0dd5a95dc3&query=Minsk')
+	.then(response => response.json())
+	.then(response => {
+        
+        console.log(response.current.weather_descriptions[0])
+        if(response.current.weather_descriptions[0] === "Sunny")
+            weatherIcon.classList.add('owf-800')
+    })
+	.catch(err => console.error(err));
+
+
     getTime()
     getDate()
     textLS.placeholder = "Type name here..";
     textLS.value = localStorage.getItem("text")
+    textCity.value = "Minsk"
+    
 }
 app()
