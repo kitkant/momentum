9408a2b986334355f8f36d993a55d35ff7d688fc
Nikita Dyshkant <58398247+kitkant@users.noreply.github.com>
2022-08-22 13:53:50 +0300

    complete weather case

--- js/app.js
+++ js/app.js
@@ -3,21 +3,31 @@ const mainDate = document.querySelector('.date')
 const greeting = document.querySelector('.greeting')
 const textLS = document.querySelector('.name')
 const textCity = document.querySelector('.city')
+//Weather
+
 const weatherIcon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temperature')
+const weatherDescription = document.querySelector('.weather-description')
+const wind = document.querySelector('.wind')
+const humidity = document.querySelector('.humidity')
+const weatherError = document.querySelector('.weather-error')
+
+//Audio
+const playerIcon = document.querySelectorAll('.player-icon')
+
 const lang = navigator.language.split('-')[0]
+
+const musicQueue = {
+    queue: [0, 1, 2, 3],
+    enqueue: function(item) {this.queue.push(item);},
+    dequeue: function() { return this.queue.shift();}
+}
+
 const timeOfDay = {
     'en': ['Good morning', 'Good afternoon', 'Good evening', 'Good night'],
     'ru': ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Спокойной ночи'],
     'pl': ['Добрай раніцы', 'Добры дзень', 'Добры вечар', 'Дабранач']
 }
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '88ac635dd6msh5e71c8b0bc6ad3ep1d67cajsnf9070ab93bf8',
-		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
-	}
-};
-
 
 const APIKEY = 'bcd4c9e2298e5fe6d8112c0dd5a95dc3'
 const getTime = () =>{
@@ -58,14 +68,8 @@ const getTime = () =>{
 
 const getTimeOfDay = (e) => greeting.innerHTML = timeOfDay[lang][e] 
 
-const setTextLocalStorage = event =>{
-    localStorage.setItem('text', event)
-    console.log('complete text', localStorage.getItem("text"))
-}
-const setCityLocalStorage = event =>{
-    localStorage.setItem('city', event)
-    console.log('complete city', localStorage.getItem("city"))
-}
+const setTextLocalStorage = event => localStorage.setItem('text', event)
+const setCityLocalStorage = event => localStorage.setItem('city', event)
 
 const getDate = () =>{
     const now = new Date();
@@ -73,23 +77,65 @@ const getDate = () =>{
     mainDate.innerHTML = now.toLocaleDateString(lang, options)
 }
 
-function app(){
-    fetch('http://api.weatherstack.com/current?access_key=bcd4c9e2298e5fe6d8112c0dd5a95dc3&query=Minsk')
+const getDateWeater = () =>{
+    fetch(`http://api.weatherstack.com/current?access_key=bcd4c9e2298e5fe6d8112c0dd5a95dc3&query=${localStorage.getItem("city")}`)
 	.then(response => response.json())
 	.then(response => {
         
-        console.log(response.current.weather_descriptions[0])
-        if(response.current.weather_descriptions[0] === "Sunny")
-            weatherIcon.classList.add('owf-800')
+        console.log(response)
+        weatherError.innerHTML = ''
+        weatherIcon.src = response.current.weather_icons[0]
+        temperature.innerHTML = response.current.temperature + '°C'
+        weatherDescription.innerHTML = response.current.weather_descriptions[0]
+        wind.innerHTML = 'Wind speed: ' + response.current.wind_speed + 'm/s'
+        humidity.innerHTML = 'Humidity: ' + response.current.humidity + '%'
     })
-	.catch(err => console.error(err));
+	.catch(err => {
+        weatherIcon.src = ''
+        temperature.innerHTML = ''
+        weatherDescription.innerHTML = ''
+        wind.innerHTML = ''
+        humidity.innerHTML = ''
+        weatherError.innerHTML = `Error! city not found for '${textCity.value}'!`
+    });
+}
 
+const playMusic = ( ) =>{
 
+}
+const pauseMusic = ( ) =>{
+
+}
+
+function app(){
+   
     getTime()
     getDate()
     textLS.placeholder = "Type name here..";
     textLS.value = localStorage.getItem("text")
-    textCity.value = "Minsk"
-    
+    if(localStorage.getItem("city") === '')
+        setCityLocalStorage('Minsk')
+    textCity.value = localStorage.getItem("city")
+    getDateWeater()
+   
+    playerIcon.forEach((event) =>{
+        event.addEventListener('click', e =>{
+           
+            if(e.target.classList[1] === 'play')
+            { 
+                e.target.classList.remove('play') 
+                e.target.classList.add('pause')
+            } else if(e.target.classList[1] === 'pause')
+            {
+                e.target.classList.add('play') 
+                e.target.classList.remove('pause')
+            }
+        })
+    })
+
 }
+
+textCity.addEventListener('change', getDateWeater)
+
+
 app()
